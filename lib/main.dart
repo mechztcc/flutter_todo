965b0d14b2bf2096117960493e7420b6db92fa04
mechztcc <dojathan@gmail.com>
2022-02-02 12:51:45 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo/app/app_module.dart';
+import 'package:flutter_todo/app/app_widget.dart';
+
+void main() {
+  runApp(AppModule());
+}
